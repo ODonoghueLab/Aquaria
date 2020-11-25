cd3196ffb3fec21e1be99b086451f2c1c3b1d1ad
Sikta, Neblina (Garvan Institute) <n.sikta@garvan.org.au>
2020-11-25 12:34:23 +1100

    fetch pdb from pdbj and remove feature description from jolecule

--- src/legacy/data-server.js
+++ src/legacy/data-server.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+import pako from './pako'
 export default class DataServer {
 
     constructor(url, id) {
@@ -6,7 +8,16 @@ export default class DataServer {
     }
 
     getProteinData(callback) {
-        fetch(this.url).then(res => res.text()).then(pdbText => callback({ pdbId: this.pdbId, pdbText }));
+        // fetch(this.url).then(res => res.text()).then(pdbText => callback({ pdbId: this.pdbId, pdbText }));
+        axios({
+			method: 'get',
+			url: this.url,
+			responseType: 'arraybuffer',
+			'accept-encoding': 'gzip'
+		  }).then(function(response){
+            const pdbText = pako.inflate(response.data, {to:"string"});
+            callback({ pdbId: window.AQUARIA.currentMember.pdb_id , pdbText })
+			})
     }
 
     getViews (callback) {
